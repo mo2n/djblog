59f2083a7793c7829ac80299afba861fc37bd7d0
mo2n <giuman.gianluca@gmail.com>
2023-11-28 12:51:08 +0100

    04-add-comment-feature-to-post

--- awwblog/blog/models.py
+++ awwblog/blog/models.py
@@ -20,10 +20,12 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
+    
+    image = models.ImageField(upload_to='featured_image/%Y/%m/%d/', blank=True, null=True) #this
+
     author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
-    #body = models.TextField()
-    image = models.ImageField(upload_to='featured_image/%Y/%m/%d/')
-    body=RichTextUploadingField()
+    # body = models.TextField() # remove this 
+    body=RichTextUploadingField() # add this
 
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -33,12 +35,40 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-publish',)
-    
+
     def __str__(self):
         return self.title
     
-    objects = models.Manager() # The default manager
-    published = PublishedManager() # Our custom manager
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',args=[self.slug])
+    
+    # to get comment with parent is none and active is true, we can use this in template
+    def get_comments(self):
+        return self.comments.filter(parent=None).filter(active=True)
+
+
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
+    body = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+    
+    def __str__(self):
+        return self.body
+
+    def get_comments(self):
+        return Comment.objects.filter(parent=self).filter(active=True)
+    
--- awwblog/blog/views.py
+++ awwblog/blog/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from .forms import CommentForm
+from django.views.generic import ListView, DetailView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
     posts = Post.published.all()
-    
+
     paginator = Paginator(posts, 10) # 10 posts in each page
     page = request.GET.get('page')
     try:
@@ -18,6 +20,51 @@ def post_list(request):
         
     return render(request,'post_list.html',{'posts':posts, page:'pages'})
 
+class PostDetail(DetailView):
+    model = Post
+    context_object_name = "post"
+    template_name = "post_detail.html"
+
 def post_detail(request, post):
     post=get_object_or_404(Post,slug=post,status='published')
-    return render(request, 'post_detail.html',{'post':post})
+
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+            return redirect(post.get_absolute_url()+'#'+str(new_comment.id))
+    else:
+        comment_form = CommentForm()
+
+        return render(request, 'post_detail.html',{'post':post,'comments': comments,'comment_form':comment_form})
+
+# handling reply, reply view
+def reply_page(request):
+    if request.method == "POST":
+
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            post_id = request.POST.get('post_id')  # from hidden input
+            parent_id = request.POST.get('parent')  # from hidden input
+            post_url = request.POST.get('post_url')  # from hidden input
+
+            reply = form.save(commit=False)
+    
+            reply.post = Post(id=post_id)
+            reply.parent = Comment(id=parent_id)
+            reply.save()
+
+            return redirect(post_url+'#'+str(reply.id))
+
+    return redirect("/")
